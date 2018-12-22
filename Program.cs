754e0a41cc497369d6f283b4365496a1f713ad97
Aziox <azioxgaming@hotmail.com>
2018-12-22 16:38:59 +0100

    fin combat tuto

--- Program.cs
+++ Program.cs
@@ -55,10 +55,7 @@ namespace tpNoel
             int money = 1; //argent du joueur pourra être utilisé 
             int round = 0; // Pour affiché le nombre de tour dans le combat 
             int weaponId = 1; //id de l'arme en notre possesion dont les infos sont dans le tableau 
-            // int healthMob;
-            // int powerMob;
-            // int giveXp;
-            // int givePo;
+            string localisation = "";// va être trés utile pour la suite car je vais gérer tout grace a ça
             string username = "";
             string gender = "";
             string duelAction = "Action disponible : Attaquer(lettre \"a\") se soigner(lettre \"h\") se reposer(lettre \"d\")";
@@ -73,37 +70,38 @@ namespace tpNoel
             //déclaration de la fonction showProfil qu'on réutilisera pour tout les cbt etc ...
             void showProfil() //déclaration de la fonction showProfil qu'on réutilisera pour tout les cbt etc ...
             {
+                Console.SetCursorPosition(0, 1);
                 Console.WriteLine("Information sur votre personnage");
-                Console.SetCursorPosition(3, 2);
+                Console.SetCursorPosition(3, 3);
                 Console.WriteLine(username);
-                Console.SetCursorPosition(1, 3);
-                Console.WriteLine("Vie : " + health);
                 Console.SetCursorPosition(1, 4);
-                Console.WriteLine("endurance : " + stamina);
+                Console.WriteLine("Vie : " + health);
                 Console.SetCursorPosition(1, 5);
-                Console.WriteLine("puissance : " + power);
+                Console.WriteLine("endurance : " + stamina);
                 Console.SetCursorPosition(1, 6);
-                Console.WriteLine("PO : " + money + "  XP : " + xp + "/100  level : " + level);
+                Console.WriteLine("puissance : " + power);
                 Console.SetCursorPosition(1, 7);
+                Console.WriteLine("PO : " + money + "  XP : " + xp + "/100  level : " + level);
+                Console.SetCursorPosition(1, 8);
                 Console.WriteLine("Votre arme : " + weapon[weaponId, 0]);
             }
             //déclaration de la fonction permettant d'affiché les informations sur les ennemis 
             void showMob()
             {
 
-                Console.SetCursorPosition(76, 0);
+                Console.SetCursorPosition(76, 1);
                 Console.WriteLine("Information sur votre ennemie");
-                Console.SetCursorPosition(79, 2);
+                Console.SetCursorPosition(79, 3);
                 Console.WriteLine(mobs.name);//nom du mob
-                Console.SetCursorPosition(77, 3);
-                Console.WriteLine("Vie : " + mobs.mobHealth); //vie du mob
                 Console.SetCursorPosition(77, 4);
-                Console.WriteLine("Puissance : " + mobs.mobPower); //puissance du mob
+                Console.WriteLine("Vie : " + mobs.mobHealth); //vie du mob
                 Console.SetCursorPosition(77, 5);
-                Console.WriteLine("gain d'xp : " + mobs.giveXp + "xp"); //gain d'xp a la fin du combat 
+                Console.WriteLine("Puissance : " + mobs.mobPower); //puissance du mob
                 Console.SetCursorPosition(77, 6);
-                Console.WriteLine("gain d'or : " + mobs.givePo + "PO"); //gain en po a la fin du combat 
+                Console.WriteLine("gain d'xp : " + mobs.giveXp + "xp"); //gain d'xp a la fin du combat 
                 Console.SetCursorPosition(77, 7);
+                Console.WriteLine("gain d'or : " + mobs.givePo + "PO"); //gain en po a la fin du combat 
+                Console.SetCursorPosition(77, 8);
                 Console.WriteLine("attaque principal : " + mobs.mobAction); //attaque de base du monstre 
             }
             //partie visible au lancement du jeu
@@ -198,7 +196,7 @@ namespace tpNoel
                         Console.WriteLine("\n\r Vous courrez droit vers " + mobs.name + " face a votre bravour il vous laisse faire et ne vous attaque pas");
                         Console.ResetColor();
                     }
-                    startBattle = true;  
+                    startBattle = true;
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("\n\r Vous avez infligé " + realPower + " dégats au pére noël sombre");
                     Console.ResetColor();
@@ -225,28 +223,28 @@ namespace tpNoel
                     if (gender == "femme")
                     {
                         Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("Le" + mobs.name + "Vous a vue dormir et en profité il n'a pu résisté a votre beauté mais n'étant pas doué il vous a blessé et réveillé ...");
+                        Console.WriteLine("Le " + mobs.name + " Vous a vue dormir et en profité il n'a pu résisté a votre beauté mais n'étant pas doué il vous a blessé et réveillé ...");
                         Console.ResetColor();
                     }
                     else
                     {
                         Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("\n\r Le" + mobs.name + "Vous a vue dormir et décide de se venger avec une petite claque");
+                        Console.WriteLine("\n\r Le " + mobs.name + " Vous a vue dormir et décide de se venger avec une petite claque");
                         Console.ResetColor();
                     }
-                    if(health <= 0)
+                    if (health <= 0)
                     {
                         Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("\n\r Vous avez perdu 90 points d'endurance car vous avez courru ..");
+                        Console.WriteLine("\n\r Game OVER");
                         Console.ResetColor();
                     }
-                    startBattle = true;                  
+                    startBattle = true;
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("\n\r Vous avez récupérer la totalité de votre endurance ");
                     Console.ResetColor();
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("\n\r Le " + mobs.name + "Vous a infligé " + mobs.mobPower + "Dégats");
+                    Console.WriteLine("\n\r Le " + mobs.name + " Vous a infligé " + mobs.mobPower + "Dégats");
                     Console.ResetColor();
                 }
                 else
@@ -257,6 +255,65 @@ namespace tpNoel
                     Console.ResetColor();
 
                 }
+                Console.WriteLine("\n\r Aie il ne vous reste plus que 1 points de vue une mouche pourrais vous tuer ");
+                startBattle = false;
+                while (!startBattle)
+                {
+
+                    Console.WriteLine("\n\r Pour le prochain tour on va se soigner attention se soigner coute 25 points d'endurance  ");
+                    Console.WriteLine(duelAction);
+                    Console.Write("Votre action : ");
+                    actionOn = Console.ReadLine();
+                    if (actionOn == "h")
+                    {
+                        Console.Clear();
+                        stamina = stamina - 25; // on réduit la stamina comme prévu
+                        health = health + maxHealth * (75 / 100);//le heal rend uniquement 75% de la vie max exemple sur 100 si on a 10pv et qu'on utilise le heal on aura donc 85 pv 
+                        if (health > maxHealth)
+                        {
+                            health = maxHealth;//si la vie dépasse alors on donne juste la valeur maximum a health pour éviter d'avoir une vie supérieur a la vie max
+                        }
+                        showProfil();
+                        showMob();
+                        Console.WriteLine("Vous vous êtes soigner et le " + mobs.name + "en a eu sa claque il est partie Vous avez gagner le combat");
+                        Console.WriteLine("Vous avez gagner " + mobs.giveXp + "xp et  " + mobs.givePo + "PO!!");
+                        xp = xp + mobs.giveXp;
+                        if (xp >= 100)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Félicitation vous avez passer un niveau");
+                            Console.ResetColor();
+                            level = level + 1;
+                            maxHealth = maxHealth + 10;
+                            power = power + 2;
+                            health = maxHealth;
+                            xp = xp - 100;
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n\r Vos statistique ont été améliorer ");
+                            Console.ResetColor();
+                        }
+                        Console.Write("passer a la suite ? (oui/non)");
+                        while (!startBattle)
+                        {
+
+                            if (Console.ReadLine() == "oui")
+                            {
+                                startBattle = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Vous devriez vraiment vous soigner");
+                        Console.ResetColor();
+                    }
+                }
+                Console.Clear();
+                showProfil();
+                localisation = "village";
             }
         }
     }
